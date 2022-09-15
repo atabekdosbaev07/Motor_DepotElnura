3fe5bb22b449db9b53a70b7899f3d4e34b83df0b
atabekdosbaev07 <dosbaeatabek07@gmail.com>
2022-09-15 08:44:18 +0300

    Create README.md

--- src/com/peaksoft/TruckService.java
+++ src/com/peaksoft/TruckService.java
@@ -1,15 +1,6 @@
 package com.peaksoft;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 
 import static com.peaksoft.Main.*;
